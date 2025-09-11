b7bbf032e11f5269c0fcf1fdf8318e0a36775ae7
Winslow <winslow@clearcreeksci.com>
2025-09-10 23:45:58 -0400

    Got rid of hard-coded app secret

--- databrowser/__init__.py
+++ databrowser/__init__.py
@@ -7,10 +7,10 @@ from ccs_dlconfig import config
 VERSION = 1
 
 app = Flask('DataBrowser')
-app.config['SECRET_KEY'] = '808524e95e823a592019dc45e8101fe3244220eeff2bde0b031fb712fb8e188f'
 
 datapath = '/opt/ccs/WeatherLogger/data'
 
 cfg = config.Settings()
+app.config['SECRET_KEY'] = cfg.secret
 
 from databrowser import routes
