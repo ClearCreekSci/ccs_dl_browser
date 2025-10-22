8d43afeb34e65e8e0c8d3f41788a61e6f8734c8f
Winslow <winslow@clearcreeksci.com>
2025-10-21 22:01:19 -0400

    Remove debug, switch to port 8080

--- run.py
+++ run.py
@@ -27,6 +27,6 @@ if '__main__' == __name__:
     if len(cfg.passwd) == 0:
         cfg.passwd = bcrypt.generate_password_hash(defaultpassword).decode('utf-8')
         cfg.write()
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=False,host='0.0.0.0',port=8080)
 
 
