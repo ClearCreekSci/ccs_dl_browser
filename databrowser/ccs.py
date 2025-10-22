4b40b4e5975c5371d8decbcd06b124dea9c0a984
Winslow <winslow@clearcreeksci.com>
2025-10-21 21:35:08 -0400

    Add license info

--- databrowser/ccs.py
+++ databrowser/ccs.py
@@ -1,3 +1,23 @@
+```
+    ccs.py
+    OpenSCAD model for the CCS Data Logger enclosure
+
+    Copyright (C) 2025 Clear Creek Scientific
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+```
+
 CCS_AIR_TEMPERATURE_UUID    = 'a0ce0210-3bbf-11ee-89eb-00e04c400cc5'
 CCS_HUMIDITY_UUID           = 'a0ce0211-3bbf-11ee-89eb-00e04c400cc5'
 CCS_AIR_PRESSURE_UUID       = 'a0ce0212-3bbf-11ee-89eb-00e04c400cc5'
